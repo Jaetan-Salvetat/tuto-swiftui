d544d55fd2529f197ac5697a22709f3a33424697
Jaetan-Salvetat <gaetansalvi08@gmail.com>
2023-02-17 18:52:25 +0100

    Create pageViewController

---
+++ TutoSwift/App/UIKit/PageViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PageViewController.swift
+//  TutoSwift
+//
+//  Created by Gaetan Salvi on 17/02/2023.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+
+        return pageViewController
+    }
+
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+    }
+}
